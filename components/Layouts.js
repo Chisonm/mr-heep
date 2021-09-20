ffd1de92f327841fbb880212e025e2f3c1957a92
chisonm <danielchisonum@gmail.com>
2021-09-20 22:09:01 +0100

    hamburger menu fixed

--- components/Layouts.js
+++ components/Layouts.js
@@ -3,13 +3,14 @@ import Footer from './Footer'
 import styles from '../styles/Home.module.css'
 
 
+
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      <div className={styles.container}>
-        <main className={styles.container}>{children}</main>
-      </div>
+        <main className={styles.container}>
+          {children}
+        </main>
       <Footer />
     </>
   )
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+import Link from 'next/Link'
+import NavStyles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+        <nav className={NavStyles.bg}>
+            <div className={NavStyles.container}>
+                <div className={NavStyles.d__flex}>
+                    <Link href="/">Logo</Link>
+                    <ul className={isOpen === false ? NavStyles.navItem__wrapper : NavStyles.navItem__wrapper +' '+ NavStyles.active } onClick={handleClick}>
+                        <li className={NavStyles.navItem__links}>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className={NavStyles.navItem__links}>
+                            <Link href="/about">About Us</Link>
+                        </li>
+                        <li className={NavStyles.navItem__links}>
+                            <Link href="/service">Services</Link>
+                        </li>
+                        <li className={NavStyles.navItem__button, NavStyles.navItem__links}>
+                            <Link href="/contact">contact</Link>
+                        </li>
+                    </ul>
+
+                    <div className={isOpen === false ? NavStyles.menu : NavStyles.menu+' '+NavStyles.active } onClick={handleClick}>
+                        <span className={NavStyles.bar}></span>
+                        <span className={NavStyles.bar}></span>
+                        <span className={NavStyles.bar}></span>
+                    </div>
+                </div>
+
+            </div>
+        </nav>
     )
 }
 
